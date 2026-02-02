a0d361d9969320421878c3d542cc6aedac9f07fd
HikariNM <nicole.hikari.mascellino@gmail.com>
2026-02-02 22:00:21 +0100

    lista prodotti

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,8 +3,39 @@ import axios from "axios"
 import 'bootstrap/dist/css/bootstrap.css';
 
 function Products() {
+
+    // State che contiene la lista dei prodotti
+    const [products, setProducts] = useState([]);
+
+    function getData() {
+        const apiUrl = 'https://fakestoreapi.com/products'
+
+        axios.get(apiUrl).then(res => {
+            console.log('Dati arrivati:', res.data)
+            setProducts(res.data)
+        }).catch(error => {
+            console.log('Si è rotto qualcosa...', error.message);
+        })
+    }
+
+    useEffect(getData, [])
     return <>
-        Products
+        <div className='container my-4'>
+            <div className='row g-4 container justify-content-center'>
+                {products.map((product) => (
+
+                    <div key={product.id} className="card bg-dark text-white col-12 col-md-3" >
+                        <img src={product.image} className="card-img-top card-img-fixed" alt={product.title} />
+                        <div className="card-body">
+                            <h3 className="card-title">{product.title}</h3>
+                            <p className="card-text">{product.price}€</p>
+                            <p className="card-text">{product.category}</p>
+                        </div>
+                    </div>
+                ))}
+
+            </div >
+        </div >
     </>
 }
 
